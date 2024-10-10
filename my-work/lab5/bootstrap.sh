88a28b3d6fd0174c07f4d48f01a3a730c62fb855
Brian Hockett <mgh2xx@virginia.edu>
2024-10-10 11:13:59 -0400

    bootstraping script

--- my-work/lab5/bootstrap.sh
+++ my-work/lab5/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/usr/bin/apt update -y
-/usr/bin/apt upgrade -y
-/usr/bin/apt install git python3 python3-pip jq -y
-/usr/bin/pip3 install boto3
+/usr/bin/yum update -y
+/usr/bin/yum install -y git python3 python3-pip jq
+/usr/bin/python3 -m pip install boto3
